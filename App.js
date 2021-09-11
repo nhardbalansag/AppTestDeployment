4901bdfbe8ca6cf688a41ff42c090b59f071170b
nhardbalansag <nhardbalansag@gmail.com>
2021-09-12 04:36:11 +0800

    modify branch with code push terminal

---
+++ App.js
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+ import React from 'react';
+ import {
+   SafeAreaView,
+   ScrollView,
+   StatusBar,
+   StyleSheet,
+   Text,
+   useColorScheme,
+   View,
+ } from 'react-native';
+ 
+ 
+ const App = () => {
+   return (
+     <SafeAreaView>
+       <View style={[{backgroundColor
+       :"white", height: "100%"}]}>
+         <Text style={[{color:"black"}]}>update 2 with code push release terminal but error on building localy</Text>
+       </View>
+     </SafeAreaView>
+   );
+ };
+ 
+ export default App;
